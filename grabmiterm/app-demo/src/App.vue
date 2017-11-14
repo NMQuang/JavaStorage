0238ad3cc483d70f06d0ce9d529f0d2c8439bccc
Minh Quang <minhquangnguyen1996@gmail.com>
2017-11-14 23:49:37 +0700

    commit code, add with firebase

--- grabmiterm/app-demo/src/App.vue
+++ grabmiterm/app-demo/src/App.vue
@@ -1,13 +1,66 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <div class="container-fluid" id="app">
+      <div class="row">
+          <div class="col-md-4">
+              <div class="alert alert-info" role="alert">
+                  <h4>Thông tin khách hàng</h4>
+              </div>
+              <div class="list-group">
+                  <a href="#" class="list-group-item" v-for="point in points">
+                      <span class="pull-right" title="remove KH" @click="deleteNote(point.id)">
+              <span class="glyphicon glyphicon-remove"></span>
+                      </span>
+                      <h4 class="list-group-item-heading">{{point.place}}</h4>
+                      <p class="list-group-item-text">
+                          {{point.type}}
+                      </p>
+                  </a>
+              </div>
+          </div>
+          <div class="col-md-4">
+              <form>
+                  <legend>Đặt Grab</legend>
+                  <div class="form-group">
+                      <label for="txtNoteTitle">Địa điểm đón</label>
+                      <input id="txtNoteTitle" type="text" class="form-control" placeholder="Nhập địa điểm" v-model="newNoteTitle">
+                  </div>
+                  <div class="form-group">
+                      <label for="txtNoteContent">Loại xe</label>
+                      <input id="txtNoteContent" type="text" class="form-control" placeholder="Nhập loại xe" v-model="newNoteContent">
+                  </div>
+                  <button type="button" class="btn btn-primary btn-block" @click="addNote">
+            <span class="glyphicon glyphicon-ok"></span>
+            Save
+          </button>
+              </form>
+          </div>
+      </div>
+    </div>
 </template>
 
 <script>
+
+import Firebase from 'firebase'
+
+let config = {
+  apiKey: 'AIzaSyBDOa513-q8BmPHmm1zlurdkkot3Z3bX-w',
+  authDomain: 'grab-midterm.firebaseapp.com',
+  databaseURL: 'https://grab-midterm.firebaseio.com',
+  projectId: 'grab-midterm',
+  storageBucket: 'grab-midterm.appspot.com',
+  messagingSenderId: '495408687232'
+}
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+
+let pointsRef = db.ref('points')
+
 export default {
-  name: 'app'
+  name: 'app',
+  firebase: {
+    points: pointsRef
+  }
 }
 </script>
 
--- grabmiterm/app-demo/src/main.js
+++ grabmiterm/app-demo/src/main.js
@@ -1,56 +1,18 @@
 import Vue from 'vue'
 import VueFire from 'vuefire'
+import App from './App'
 
-Vue.user(VueFire)
+Vue.use(VueFire)
 
-
+/* eslint-disable no-new */
+/* eslint indent: ["error", "tab"] */
+/* eslint indent: ["error", 4, { "VariableDeclarator": 1 }] */
+/* eslint-env es6 */
+/* eslint-disable eol-last */
+/* eslint-env es6 */
+/* eslint no-extra-semi: "error" */
 new Vue({
     el: '#app',
-    data: {
-        notes: [],
-        newNoteTitle: '',
-        newNoteContent: ''
-    },
-
-    mounted: function() {
-        var self = this;
-        $.ajax({
-            url: 'data/data.json',
-            dataType: 'json',
-            timeout: 10000
-        }).then(function(data) {
-            self.notes = data;
-        }).fail(function(j, status, err) {
-            console.log(err);
-        });
-    },
-
-    methods: {
-        addNote: function() {
-            var self = this;
-            var newNote = {
-                title: self.newNoteTitle,
-                content: self.newNoteContent
-            };
-            self.notes.push(newNote);
-
-            self.newNoteTitle = '';
-            self.newNoteContent = '';
-        },
-        deleteNote: function(d_title) {
-            var self = this;
-            var rs = confirm('delete note?');
-            if (rs) {
-                var d_idx = -1;
-                $.each(self.notes, function(idx, value) {
-                    if (value.title === d_title) {
-                        d_idx = idx;
-                        return;
-                    }
-                });
-
-                self.notes.splice(d_idx, 1);
-            }
-        },
-    }
-});
+    template: '<App/>',
+    components: { App }
+})
